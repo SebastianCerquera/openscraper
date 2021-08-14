9cb179f1e52210b2e584041e67af4b88b3e2cc62
Sebastian Gonzalez <sebastian2zen@gmail.com>
2021-08-14 13:51:22 -0500

    the cypress backbone is working

--- cypress/integration/openscraper.spec.js
+++ cypress/integration/openscraper.spec.js
@@ -5,10 +5,16 @@ describe('Pagination listings: ', () => {
     var infiniteListing = null
     
     beforeEach(() => {
+        cy.visit('http://localhost:9090/app/')
+        
         infiniteListing = new openscraper.InfiniteListing(null, 1000)
     })
     
     it('scrolls down up to the end of the page', () => {
+        cy.window().then((win) => {
+            win.scrollTo(5000, 5000)    
+        })
+        
         expect(infiniteListing != null)
     })
 })
