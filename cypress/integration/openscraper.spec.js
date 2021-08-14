0c2d745f870619223547eefefe55c80dbace4e9c
Sebastian Gonzalez <sebastian2zen@gmail.com>
2021-08-14 17:36:52 -0500

    the stubbing is not working

--- cypress/integration/openscraper.spec.js
+++ cypress/integration/openscraper.spec.js
@@ -1,20 +1,82 @@
 const openscraper = require('../../extension/openscraper')
 
-describe('Pagination listings: ', () => {
+describe('Page listings', () => {
 
-    var infiniteListing = null
-    
-    beforeEach(() => {
-        cy.visit('http://localhost:9090/app/')
+    describe('Base class', () => {
+        var extractor = null
         
-        infiniteListing = new openscraper.InfiniteListing(null, 1000)
-    })
-    
-    it('scrolls down up to the end of the page', () => {
-        cy.window().then((win) => {
-            win.scrollTo(5000, 5000)    
+        beforeEach(() => {
+            extractor = new openscraper.PostExtractor()
         })
         
-        expect(infiniteListing != null)
+        it('The traverse should finish right away if there are no links', () => {
+            // given
+            var extractorStubName = "extractorStubName"
+            
+            // This tells the component that there are no links
+            openscraper.PageListing.prototype.hasLinks = function(){
+                return false
+            }
+            
+            cy.stub(extractor, 'extract').as(extractorStubName)
+            
+            // when: 
+            var pageListing = new openscraper.PageListing(extractor)
+            pageListing.traverse()
+
+            // then:
+            cy.get("@" + extractorStubName).should('not.have.been.called')
+        })
+
+        
+        it('The traverse should find only tow results and then complete', () => {
+            // given
+            var extractorStubName = "extractorStubName"
+
+            // This tells the component that there are no links
+            var counter = 0
+            openscraper.PageListing.prototype.hasLinks = function(){
+                counter++
+                if(counter < 2)
+                    return true
+                return false
+            }
+            
+            openscraper.PageListing.prototype.nextLink = function(){
+                return {}
+            }
+            
+            cy.stub(extractor, 'extract').as(extractorStubName)
+            
+            // when: 
+            var pageListing = new openscraper.PageListing(extractor)
+            pageListing.traverse()
+
+            // then:
+            cy.get("@" + extractorStubName).should('have.been.calledTwice')
+        })
     })
+    
+    describe('Infinite scroll', () => {
+        var infiniteListing = null
+        
+        beforeEach(() => {
+            infiniteListing = new openscraper.InfiniteListing(null, 1000)
+        })
+        
+        it('Builds the listing, the completed state is false by default', () => {
+            expect(infiniteListing != null)
+            expect(infiniteListing.timeout == 1000)
+            expect(infiniteListing.counter == 0)
+            expect(!infiniteListing.completed)
+        })
+        
+        it('hasLinks is false after completed', () => {
+            expect(infiniteListing.hasLinks())
+
+            infiniteListing.completed = true
+            expect(!infiniteListing.hasLinks())
+        })        
+    })
+    
 })
